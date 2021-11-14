cf41fdf3448fdf79830225b6374f9e6bb0c7e601
A01704868 <A01704868@itesm.mx>
2021-11-13 18:58:39 -0600

    part 6

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -6,9 +6,10 @@ import Home from './components/frontend/Home';
 import Login from './components/frontend/auth/Login';
 import Register from './components/frontend/auth/Register';
 import axios from 'axios';
+import AdminPrivateRoute from './AdminPrivateRoute';
 
 
-axios.defaults.baseURL = "http://localhost:8000/"
+axios.defaults.baseURL = window.apiURL
 axios.defaults.headers.post['Content-Type'] = 'application/json';
 axios.defaults.headers.post['Accept'] = 'application/json';
 axios.defaults.withCredentials = true;
@@ -34,7 +35,10 @@ function App() {
             {localStorage.getItem('auth_token') ? <Redirect to='/' /> : <Register />}
           </Route>
 
-          <Route path="/admin" name="Admin" render={(props) => <MasterLayout {...props} />} />
+          {/*<Route path="/admin" name="Admin" render={(props) => <MasterLayout {...props} />} />*/}
+
+          <AdminPrivateRoute  path="/admin" name="Admin" />
+
         </Switch>
       </Router>
     </div>
