f571beff133131283ec6f5fd7e5a858a8d9e903d
feeramezquita <a01421954@itesm.mx>
2021-11-14 17:25:28 -0600

    Laravel & React working together

---
+++ frontend/src/components/admin/Producto.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+
+function Producto(props) {
+    return (
+        <tr>
+            <th>{props.id}</th>
+            <td>{props.nombre}</td>
+            <td>{props.precio}</td>
+            <td>{props.cantidad}</td>
+            <td>
+                <Link to="/admin/EditProduct" className="btn btn-success btn-sm">Editar</Link>
+            </td>
+            <td>
+                <Link to="#" className="btn btn-danger btn-sm">Eliminar</Link>
+            </td>
+        </tr>
+    )
+}
+
+export default Producto
