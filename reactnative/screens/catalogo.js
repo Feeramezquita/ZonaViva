24a7ba94f28075d33afeb126f48ace5700f3749c
JesusOlmosLarios <69598051+JesusOlmosLarios@users.noreply.github.com>
2021-11-24 23:26:38 -0600

    Minor fixes ready to work

--- reactnative/screens/catalogo.js
+++ reactnative/screens/catalogo.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { View, Text, TouchableOpacity, Image, ScrollView} from 'react-native';
 import { StatusBar } from 'expo-status-bar';
 import { globalStyles } from '../styles/global';
@@ -8,7 +8,7 @@ export default function Catalogo({navigation}){
         <View style={globalStyles.containergreen}>
              <ScrollView>
             <StatusBar style="auto" />
-            <Text style={globalStyles.titlecatalogo}>Conocer nuestro catalogo de productos</Text>
+            <Text style={globalStyles.titlecatalogo}>Conoce nuestros productos</Text>
             <View style={globalStyles.productos}>
                 <Text style={globalStyles.parrafosbutum}>Producto1</Text>
                 <Image style={globalStyles.imagenproducto} source={require("../assets/img/productos.jpeg")}></Image>
